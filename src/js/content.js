2603ace8fbc91e3fbc915c5cdd76a6dfcc5ca6a8
Mauro <mauro.guerrero@glorifi.com>
2023-05-03 12:25:54 -0400

    Feat - Add inital no task button

--- src/js/content.js
+++ src/js/content.js
@@ -1,5 +1,22 @@
 import { createElementType } from "./Components/utils";
 
 
+ const noTask = {
+    main: createElementType('noTaskDiv', 'div'),
+    button: createElementType('noTaskBtn', 'button'),
+    header: createElementType('noTaskH3', 'h3')
+ }
+
+noTask.header.textContent = 'Create your first ToDo'
+noTask.main.appendChild(noTask.header)
+noTask.main.appendChild(noTask.button)
+
+
+
+
+
+
 
 export const contentSection  = createElementType('content', 'div')
+
+contentSection.appendChild(noTask.main)
